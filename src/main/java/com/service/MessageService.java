a4b72f3c47cb26caa1e0ba2706c90cd9d931491d
Pakapol Piritaburana <pakapolpiritaburana@Pakapol-Piritaburana.local>
2017-02-25 17:34:55 +0700

    delete dao

--- src/main/java/com/service/MessageService.java
+++ src/main/java/com/service/MessageService.java
@@ -1,30 +1,32 @@
 package com.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 
-import com.dao.MessageDao;
 import com.domain.Request;
 import com.domain.Response;
 
 @Service
 public class MessageService {
-	@Autowired
-	private MessageDao messageDao;
-	
 	@ResponseBody
 	public Response addSlackNoti(Request slackRequest) {
-		String responseSent = messageDao.send(slackRequest);
-		slackRequest.setText(responseSent);
-		Response response = new Response();
-		if (responseSent.equalsIgnoreCase("ok")) {
-			response.setResponse_code("0");
-			response.setResponse_msg("SUCCESS");
-		} else {
-			response.setResponse_code("0000");
-			response.setResponse_msg("FAIL");
+		try {
+			RestTemplate restTemplate = new RestTemplate();
+			String response = restTemplate.postForObject("https://hooks.slack.com/services/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", slackRequest,String.class);
+			Response buildResponse = new Response();
+			if (response.equalsIgnoreCase("ok")) {
+				buildResponse.setResponse_code("0");
+				buildResponse.setResponse_msg("SUCCESS");
+			} else {
+				buildResponse.setResponse_code("0000");
+				buildResponse.setResponse_msg("FAIL");
+			}
+			return buildResponse;
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		    System.out.println(e.getCause().getMessage());
+			throw new RuntimeException(e);
 		}
-        return response;
     }
 }
